51cfb9cc44b6a0be90534728438409811274a0b8
jonathan0x113c1c3 <jonathanof2016@gmail.com>
2026-06-27 21:32:32 +0700

    ...

--- something_os/init.nix
+++ something_os/init.nix
@@ -18,13 +18,7 @@ mkShell {
     # 2. Add the required component for OS dev (sysroot compilation)
     rustup component add rust-src
 
-    # 3. Install bootimage globally via cargo if not already present
-    if ! command -v bootimage &> /dev/null; then
-      echo "bootimage not found. Installing..."
-      cargo install bootimage
-    else
-      echo "bootimage is already installed."
-    fi
+
 
     echo "--- Environment ready! ---"
   '';
